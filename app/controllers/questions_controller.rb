2743f213ac0d233ee5d5239edb8d08568fd89dcf
luiszambrano <luisdavidzambrano@gmail.com>
2021-10-18 14:01:46 -0400

    ejercicio_2

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,14 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+  def answer
+    @answer = params[:question]
+    if @answer == "I am going to work"
+      @answer = "Great!";
+    elsif @answer[-1] == "?"
+      @answer = "Silly question, get dressed and go to work!.";
+    else @answer = "I don't care, get dressed and go to work!"
+    end
+
+  end
+end
